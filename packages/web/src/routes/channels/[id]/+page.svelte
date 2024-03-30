be9d8349f1bdc45c918aa0aef0f65dc9cf09b634
Morgan Dilling <morgandilling171@gmail.com>
2024-03-30 16:55:35 +0000

    feat: Fix emojis

--- packages/web/src/routes/channels/[id]/+page.svelte
+++ packages/web/src/routes/channels/[id]/+page.svelte
@@ -19,7 +19,7 @@
 	let chatSearchString = '';
 
 	const emojiRegex =
-		/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+		/^(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])+$/g;
 
 	if (!$authedUser) {
 		browser && goto(`/login?returnTo=${window.location.pathname}`);
