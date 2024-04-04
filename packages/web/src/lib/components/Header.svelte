18fc4d1526c8bd466e9da6de89430617405aacc0
Morgan Dilling <morgandilling171@gmail.com>
2024-04-04 09:09:20 +0800

    feat: Add link to application when signed in on homepage

--- packages/web/src/lib/components/Header.svelte
+++ packages/web/src/lib/components/Header.svelte
@@ -2,16 +2,23 @@
 	import WhisprLogoWhite from './whispr-logo-white.svelte';
 	import AnchorButton from './AnchorButton.svelte';
 	import { goto } from '$app/navigation';
+	import { authedUser } from '$lib/libWhispr';
 </script>
 
 <header>
 	<a href="/" class="logo">
 		<WhisprLogoWhite />
 	</a>
-	<div class="options">
-		<AnchorButton href="/register">Register</AnchorButton>
-		<AnchorButton href="/login">Login</AnchorButton>
-	</div>
+	{#if $authedUser}
+		<div class="options">
+			<AnchorButton href="/channels/@self">Go To App</AnchorButton>
+		</div>
+	{:else}
+		<div class="options">
+			<AnchorButton href="/register">Register</AnchorButton>
+			<AnchorButton href="/login">Login</AnchorButton>
+		</div>
+	{/if}
 </header>
 
 <style lang="scss">
