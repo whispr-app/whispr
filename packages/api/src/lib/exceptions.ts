7f7c734da2f6b51c3ec6532169869ceaee9e3dc5
Morgan Dilling <morgandilling171@gmail.com>
2024-05-20 23:20:09 +0100

    fix: Update error message

--- packages/api/src/lib/exceptions.ts
+++ packages/api/src/lib/exceptions.ts
@@ -68,7 +68,7 @@ export class DomainNotSpecified extends Error {
   constructor() {
     super();
     this.message =
-      'Domain was not specified in a production environment. Set a domain by specifying --domain=example.com whilst running (replace example.com with your domain)';
+      'Domain was not specified in a production environment. Set a domain by specifying --domain example.com whilst running (replace example.com with your domain)';
   }
 }
 
